8d293359feb6d27cff57f34628e8c885971736e1
vsr2158 <v.shekhar@yahoo.com>
2021-11-23 15:11:55 -0700

    commit4

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,11 +1,31 @@
 const app = Vue.createApp({
     data(){
         return{
-            courseGoal: 'Finish Course Vue!',
+            courseGoalA: 'Finish Course Vue - A !',
+            courseGoalB: 'Finish Course Vue - B !',
             vueLink: 'http://vuejs.org'
         }
-    }
-
-});
-
+    },
+    methods: {
+        outputGoal(){
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5 ){
+                return 'Learn Vue!';
+            }
+            else {
+                return 'Master Vue!';
+            }
+            },
+        outputData(){
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5 ){
+                return this.courseGoalA;
+            }
+            else {
+                return this.courseGoalB;
+            }
+            }            
+        }
+}
+);
 app.mount('#user-goal')
